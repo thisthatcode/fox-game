9267f1a93b367da3c4e75d5012dd698a230b6e66
thisthatcode <adjoa.darien+thisthatcode@gmail.com>
2023-06-19 10:53:54 -0400

    adding new game states

--- src/gameState.js
+++ src/gameState.js
@@ -1,14 +1,57 @@
 const gameState = {
   current: "INIT",
   clock: 1,
+  wakeTime: -1, // sentinel value, indicate not currently active
   tick() {
     this.clock++;
-    console.log('Clock', this.clock);
+    console.log("Clock", this.clock);
     return this.clock;
   },
+  startGame() {
+    console.log("hatching");
+    this.current = "HATCHING";
+    this.wakeTime = this.clock + 3;
+  },
+  wake() {
+    console.log("awoken");
+    (this.current = "IDLING"), (this.wakeTime = -1);
+  },
   handleUserAction(icon) {
-    console.log(icon);
-  }
-}
+    if (
+      ["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)
+    ) {
+      //do nothing
+      return;
+    }
 
+    if (this.current === "INIT" || this.current === "DEAD") {
+      this.startGame();
+      return;
+    }
+
+    switch (icon) {
+      case "weather":
+        this.changeWeather();
+        break;
+      case "poop":
+        this.cleanUpPoop();
+        break;
+      case "fish":
+        this.feed();
+        break;
+    }
+  },
+  changeWeather() {
+    console.log("changeWeather");
+  },
+  cleanUpPoop() {
+    console.log("cleanUpPoop");
+  },
+  feed() {
+    console.log("feed");
+  },
+};
+
+//bind gameState to the 'this'
+export const handleUserAction = gameState.handleUserAction.bind(gameState);
 export default gameState;
--- src/init.js
+++ src/init.js
@@ -1,17 +1,17 @@
-import gameState from "./gameState";
+import gameState, { handleUserAction } from "./gameState";
 import { TICK_RATE } from "./constants";
 import initButtons from "./buttons";
 
 async function init() {
   console.log("Starting game...");
-  initButtons(gameState.handleUserAction)
+  initButtons(handleUserAction);
 
   let nextTimeToTick = Date.now();
 
   function nextAnimationFrame() {
     const now = Date.now();
 
-    if(nextTimeToTick <= now) {
+    if (nextTimeToTick <= now) {
       gameState.tick();
       nextTimeToTick = now + TICK_RATE;
     }
